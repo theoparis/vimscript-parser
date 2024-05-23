669aa60bc6f131807e7f77bd3acb14747d582a3a
Theo Paris <theop@flowtr.dev>
2024-05-22 17:54:52 -0700

    feat: add function calls
    Added strings, function calls and improved number parsing.

--- src/lib.rs
+++ src/lib.rs
@@ -27,5 +27,44 @@ mod tests {
 		let result = result.unwrap().next().unwrap();
 
 		assert_eq!(result.as_rule(), Rule::statement);
+
+		let next = result.into_inner().next().unwrap();
+		assert_eq!(next.as_rule(), Rule::let_statement);
+	}
+
+	#[test]
+	fn test_call() {
+		let input = "call sign_undefine()";
+
+		let result = ExpressionParser::parse(Rule::statement, input);
+
+		if let Err(err) = result {
+			panic!("{}", err);
+		}
+
+		let result = result.unwrap().next().unwrap();
+
+		assert_eq!(result.as_rule(), Rule::statement);
+
+		let next = result.into_inner().next().unwrap();
+		assert_eq!(next.as_rule(), Rule::call_statement);
+	}
+
+	#[test]
+	fn test_call_with_arguments() {
+		let input = r#"call sdl#OpenWindow("New Window", 800, 600)"#;
+
+		let result = ExpressionParser::parse(Rule::statement, input);
+
+		if let Err(err) = result {
+			panic!("{}", err);
+		}
+
+		let result = result.unwrap().next().unwrap();
+
+		assert_eq!(result.as_rule(), Rule::statement);
+
+		let next = result.into_inner().next().unwrap();
+		assert_eq!(next.as_rule(), Rule::call_statement);
 	}
 }
